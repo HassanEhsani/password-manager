12a16b526484be408bc60ea729e530810a41b32c
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-18 22:53:28 +0300

    wrote the function load_key

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("What is the master password? ")
-
+'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)
-        
-write_key()
+        key_file.write(key)''' 
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+master_pwd = input("What is the master password? ")
+
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
 
 def view():
     with open('passwords.txt', 'r') as f:
@@ -23,7 +32,7 @@ def add():
     pwd = input("password: ")
     
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")  
+        f.write(name + "|" + fer.encrypt(pwd.encode()) + "\n")  
 
 while True:
     mode = input("would you like to add a new password or view exsiting ones (view, add), prees q to quit ? ").lower()
