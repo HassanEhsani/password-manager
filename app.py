44c0d7b51fb76d1cbc3b5c2a78eb1b218bdddceb
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-18 11:44:53 +0300

    i solved the problem with the split

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ from cryptography.fernet import Fernet
 
 master_pwd = input("What is the master password? ")
 
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+        
+write_key()
+
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
-            user,passw = data.split("|")
-            print("User:", user, "| Password:", passw) 
+            # Split using '|', but limit the split to two parts
+            user, passw = data.split("|", 1)  
+            print("User:", user, ", Password:", passw)
+
         
 def add():
     name = input('Account Name: ')
